247fdf8f61f0aea94043e94f78bf163e6b84c4ac
Najmeh Foroozani <46888580+Foroozani@users.noreply.github.com>
2021-01-24 15:37:54 +0100

    updated

--- CNN/convolutional_neural_network.py
+++ CNN/convolutional_neural_network.py
@@ -1,5 +1,5 @@
 # Convolutional Neural Network with Tensorflow
-
+# Kaggle data set
 # Importing the libraries
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
@@ -57,7 +57,7 @@ cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accura
 # Training the CNN on the Training set and evaluating it on the Test set
 cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
 
-# Part 4 - Making a single prediction
+# Part 4 - Making a single prediction --------------------------------------------------------
 import numpy as np
 from keras.preprocessing import image
 test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
@@ -67,7 +67,7 @@ result = cnn.predict(test_image)
 print('classes of samples are:\n',training_set.class_indices)
 
 if result[0][0] == 1:
-    prediction = 'Picture of a dog'
+    prediction = 'It is a picture of a dog'
 else:
-    prediction = 'Picture of a cat'
+    prediction = 'It is a picture of a cat'
 print(prediction)
